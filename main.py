78edc4cff230554b2015b8e72226021f8a83a359
Listen Compute <dceptx@icloud.com>
2026-02-14 21:44:45 -0700

    Chapter 2: Lesson 4 - dt and clock

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,8 +19,14 @@ def main():
 
         screen.fill("black")
 
+
+
+
+
         # this goes last
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
