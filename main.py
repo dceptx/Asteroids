c8c1e60429031ae81dfcc3d10b2c97867ab675a1
Listen Compute <dceptx@icloud.com>
2026-02-14 22:55:23 -0700

    Chapter 3: Lesson 2 - player.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -17,10 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+
         screen.fill("black")
 
-
-
+        player.draw(screen)
 
 
         # this goes last
