fb145a6d836392e66c89480534349039c3637cdd
Listen Compute <dceptx@icloud.com>
2026-02-15 17:20:51 -0700

    Chapter 3: Lesson 5 - Groups

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,17 +24,20 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        # player.update(dt)
+        # player.draw(screen)
+        updatable.update(dt)
+
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
 
 
         # this goes last
         pygame.display.flip()
-
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
